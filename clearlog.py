4b70af540fcfd964f35458c0085a60318e3bf98b
AlgoRL <120761514+AlgoRL@users.noreply.github.com>
2024-03-24 21:20:46 -0400

    fixing file name search for clearlog

--- clearlog.py
+++ clearlog.py
@@ -3,17 +3,17 @@ import json
 
 def clear_logs(username):
     try:
-        with open("warnings_log.json", "r+") as file:
+        with open("reports_log.json", "r+") as file:
             data = json.load(file)
             if username in data:
                 del data[username]
                 file.seek(0)
                 json.dump(data, file, indent=4)
-                print(f"Warning logs for user '{username}' cleared successfully.")
+                print(f"Report logs for user '{username}' cleared successfully.")
             else:
                 print(f"User '{username}' not found in the warning logs.")
     except FileNotFoundError:
-        print("Warning logs file not found.")
+        print("Report logs file not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
