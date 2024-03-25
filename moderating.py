e2eae5fb5a26684665743f00cecb986f5657f023
AlgoRL <120761514+AlgoRL@users.noreply.github.com>
2024-03-24 20:44:25 -0400

    fixing warnings_today thing

--- moderating.py
+++ moderating.py
@@ -178,7 +178,7 @@ async def on_message(message):
             print(f"[Log] You are immune! Member: {message.author}")
             return
         
-        print(f"Warnings today for {message.author}: {warnings_today(str(message.author.id))}")
+        print(f"Warnings today for {message.author}: {warnings_today(str(message.author))}")
         
         if any(word in message.content for word in banned_words):
             if warnings_today(str(message.author.id)) >= 3:
